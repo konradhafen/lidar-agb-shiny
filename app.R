cce8c40d171251bb1ebeb79dbd1ee8ff70963685
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-06-13 14:33:40 -0700

    added correct data and building app

--- app.R
+++ app.R
@@ -5,8 +5,12 @@ ui <- fluidPage(
   fileInput("csvfile", "Choose CSV file", buttonLabel = "Browse", placeholder = "No file selected",
             accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
   checkboxInput("header", "Header", TRUE),
-  uiOutput("variables"),
-  tableOutput("corrmat")
+  uiOutput("response"),
+  uiOutput("predictor"), 
+  actionButton("lmbutton", "Linear model"), 
+  actionButton("rfbutton", "RF model"),
+  plotOutput("model"),
+  verbatimTextOutput("summary")
 )
 
 server <- function(input, output) {
@@ -19,21 +23,45 @@ server <- function(input, output) {
     read.csv(infile$datapath, header = input$header)
   })
   
-  output$variables <- renderUI({
+  output$response <- renderUI({
     df <- filedata()
     if (is.null(df)) return(NULL)
     
-    items=names(df[,16:21])
+    items=names(df)
     names(items)=items
     selectInput("response", "Response variable",items)
     
   })
   
-  output$corrmat <- renderTable({
+  output$predictor <- renderUI({
     df <- filedata()
     if (is.null(df)) return(NULL)
     
-    cor(df[,16:21])
+    items=names(df)
+    names(items)=items
+    selectInput("predictor", "Predictor variable",items)
+    
+  })
+  
+  observeEvent(input$lmbutton, {
+    df <- filedata()
+    if (is.null(df)) return(NULL)
+    lm1 <- lm(df[,input$response] ~ df[,input$predictor])
+    lm1sum <- summary(lm1)
+    
+    output$model <- renderPlot({
+      plot(df[,input$predictor], df[,input$response], xlab=input$predictor, ylab=input$response,
+           main="Linear model fit")
+      abline(lm1$coefficients[1], lm1$coefficients[2])
+      })
+    
+    output$summary <- renderPrint({
+      summary(lm1)
+    })
+  })
+  
+  observeEvent(input$rfbutton, {
+    
   })
 }
 
